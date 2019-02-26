d4ae9d753a193c3c162a9ddda799810d66205e5d
Mohsin <mohammadmohsin@gmail.com>
2019-02-26 21:57:36 +0500

    Redirect to admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   protected
     def after_sign_in_path_for(resource)
       if resource.has_role? :admin
-        users_path
+        admin_index_path
       else
         request.env['omniauth.origin'] || stored_location_for(resource) || visitors_path
       end
