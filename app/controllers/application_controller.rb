2c45f4fb9b1e9c7e7ce9cd97c454232c2d285999
Mohsin <mohammadmohsin@gmail.com>
2019-02-26 12:00:28 +0500

    Devise Sessions views. After signin/signout paths overwritten.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  protected
+    def after_sign_in_path_for(resource)
+      request.env['omniauth.origin'] || stored_location_for(resource) || visitors_path
+    end
+
+    def after_sign_out_path_for(resource_or_scope)
+      new_user_session_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'visitors#home'
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
   resources :users
   resources :rooms
   resources :invitations
